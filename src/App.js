ff7808b12f3dd62f8bf2e33e359925079b4bf220
Cláudio de Oliveira Pereira <claudio.pereira@inpa.gov.br>
2021-10-15 13:19:21 -0400

    feat: add signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Routes from "./Routes";
 import GlobalStyles from "./styles/style";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div>
       <GlobalStyles />
       <Routes />
+      <ToastContainer />
     </div>
   );
 }
