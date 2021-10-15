3e936b0109bcfd8610c9432fc24ccc77f6cd3e49
Arthur Sarmento <arthurbs17@gmail.com>
2021-10-15 11:13:35 -0300

    feat: Home Page

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,8 +1,23 @@
+import { useHistory } from "react-router";
+import { Container, Content } from "./styles";
+
 const Home = () => {
+  const history = useHistory();
+
+  const handleNav = (path) => {
+    history.push(path);
+  };
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <Container>
+      <Content>
+        <h1>Gestão de Hábitos</h1>
+        <p>Faça seu Login ou Registre-se conosco.</p>
+        <div>
+          <button onClick={() => handleNav("/login")}>Login</button>
+          <button onClick={() => handleNav("/signup")}>Cadastra-se</button>
+        </div>
+      </Content>
+    </Container>
   );
 };
 
