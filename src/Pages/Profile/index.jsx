74020f60b151e4bff1e4cd291bc4155f1c183891
vinicius <vinapro91@gmail.com>
2021-10-17 13:19:17 -0300

    feat: Add user info in Profile Page

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -1,13 +1,67 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { GroupsContext } from "../../Providers/Groups";
 import { UserIdContext } from "../../Providers/User_id";
+import api from "../../Services/api";
 
 const Profile = () => {
-  const { userId, setUserId } = useContext(UserIdContext);
-  useEffect(() => {}, [userId]);
+  const { userId } = useContext(UserIdContext);
+  const { subscribedGroups } = useContext(GroupsContext);
 
+  const [userInfo, setUserinfo] = useState({});
+  useEffect(() => {
+    api
+      .get(`/users/${userId}/`)
+      .then((response) => setUserinfo(response.data))
+      .catch((error) => console.log(error));
+  }, [userId]);
+  const history = useHistory();
+
+  const logout = () => {
+    history.push("/");
+    localStorage.clear();
+  };
+  const showAllGroups = () => {
+    history.push("/groups");
+  };
   return (
     <>
-      <h1>Profile</h1>
+      <div>
+        <h1>Bem Vindo , {userInfo.username}</h1>
+        <button onClick={() => logout()}>Sair</button>
+      </div>
+
+      <div>
+        <h2>Grupos</h2>
+        <p>
+          <button onClick={showAllGroups}>Mostrar mais grupos</button>
+        </p>
+        <ul>
+          {subscribedGroups.map((group, index) => (
+            <li key={index}>
+              <p>Nome do grupo : {group.name}</p>
+              <p>Categoria : {group.category}</p>
+              <p>Descrição : {group.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2> Metas</h2>
+        <ul>
+          {subscribedGroups.map((group, index) => (
+            <ul key={index}>
+              {group.goals.map((goal) => (
+                <li key={index}>
+                  <p>Meta: {goal.title}</p>
+                  <p>dificuldade: {goal.difficulty}</p>
+                  <p>Progresso: {goal.how_much_achieved}</p>
+                </li>
+              ))}
+            </ul>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
--- src/Providers/User_id/index.js
+++ src/Providers/User_id/index.js
@@ -2,10 +2,17 @@ import { createContext, useState } from "react";
 export const UserIdContext = createContext();
 
 export const UserIdProvider = ({ children }) => {
-  const [userId, setUserId] = useState("");
+  const [userId, setUserId] = useState(
+    JSON.parse(localStorage.getItem("@Habits:UserId")) || ""
+  );
+
+  const addUserId = (id) => {
+    localStorage.setItem("@Habits:UserId", JSON.stringify(id));
+    setUserId(id);
+  };
 
   return (
-    <UserIdContext.Provider value={{ userId, setUserId }}>
+    <UserIdContext.Provider value={{ userId, addUserId }}>
       {children}
     </UserIdContext.Provider>
   );
